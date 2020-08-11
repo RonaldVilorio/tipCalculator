4f413410776852a60f160b4f45a941159522f4a4
RonaldVilorio <ronvilorio@gmail.com>
2020-08-11 11:14:52 -0400

    changed html datalist to select, selected html elements in js file

--- tipCalculator/script.js
+++ tipCalculator/script.js
@@ -1,1 +1,9 @@
+const bill = document.querySelector("#bill");
+const serviceLevel = document.querySelector("#serviceLevel");
+const peopleCount = document.querySelector("#peopleCount");
+const total = document.querySelector("#total");
 
+function calculate(e) {
+  e.preventDefault();
+  total.textContent = "hey";
+}
